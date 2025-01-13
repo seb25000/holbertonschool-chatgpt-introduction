031ad689cb5dfa8881a6cad78da5b03f35a62f02
seb25000 <seb25000@gmail.com>
2025-01-13 21:32:01 +0100

    checkbook.py

---
+++ debugging/checkbook.py
@@ -0,0 +1,57 @@
+class Checkbook:
+    def __init__(self):
+        self.balance = 0.0
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Deposited ${:.2f}".format(amount))
+        print("Current Balance: ${:.2f}".format(self.balance))
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds to complete the withdrawal.")
+        else:
+            self.balance -= amount
+            print("Withdrew ${:.2f}".format(amount))
+            print("Current Balance: ${:.2f}".format(self.balance))
+
+    def get_balance(self):
+        print("Current Balance: ${:.2f}".format(self.balance))
+
+def main():
+    cb = Checkbook()
+    while True:
+        action = input("What would you like to do? (deposit, withdraw, balance, exit): ").lower()
+        if action == 'exit':
+            break
+        elif action == 'deposit':
+            while True:
+                try:
+                    amount = float(input("Enter the amount to deposit: $"))
+                    if amount < 0:
+                        print("Please enter a positive amount.")
+                    else:
+                        cb.deposit(amount)
+                        break
+                except ValueError:
+                    print("Invalid input. Please enter a numeric value for the deposit amount.")
+
+        elif action == 'withdraw':
+            while True:
+                try:
+                   amount = float(input("Enter the amount to withdraw: $"))
+                   if amount < 0:
+                      print("Please enter a positive amount.")
+                   else:
+                        cb.withdraw(amount)
+                        break
+                except ValueError:
+                    print("Invalid input. Please enter a numeric value for the withdrawal amount.")
+
+        elif action == 'balance':
+            cb.get_balance()
+        else:
+            print("Invalid command. Please try again.")
+
+if __name__ == "__main__":
+    main()
